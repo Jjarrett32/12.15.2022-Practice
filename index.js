e1e6d166fcc5e606c8a32547e12a30ecf4da600e
Jabarre <jjarrett.prsvr@gmail.com>
2022-12-16 12:57:26 -0600

    new files

---
+++ index.js
@@ -0,0 +1,14 @@
+// declare a variable
+// assigh value of variable to a array
+// create a function
+// Donald Capers7:58 PM
+// nest a forloop for the array inside the function
+// log out the array
+// then outside of function call the function
+let streetNames = ["Lockard", "Lee", "Moore"]
+function streetSelector() {
+for (let i = 0; i < streetNames.length; i++){
+    console.log(streetNames[i])
+}
+}
+streetSelector()
